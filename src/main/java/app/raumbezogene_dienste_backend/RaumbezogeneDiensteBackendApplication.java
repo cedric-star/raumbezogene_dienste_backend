0540fff6a07709c5e34296e07a2cd2a4427ea959
cedric-star <cdrc.wnsch@gmail.com>
2026-08-15 22:09:22 +0200

    d

---
+++ src/main/java/app/raumbezogene_dienste_backend/RaumbezogeneDiensteBackendApplication.java
@@ -0,0 +1,13 @@
+package app.raumbezogene_dienste_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaumbezogeneDiensteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaumbezogeneDiensteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/raumbezogene_dienste_backend/RaumbezogeneDiensteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.raumbezogene_dienste_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaumbezogeneDiensteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
