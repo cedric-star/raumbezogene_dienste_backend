d11560a8d1bbc1ea393051cc9bcc129025a5491a
cedric-star <cdrc.wnsch@gmail.com>
2026-08-17 17:43:54 +0200

    template

--- src/main/java/app/raumbezogene_dienste_backend/RaumbezogeneDiensteBackendApplication.java
+++ src/main/java/app/raumbezogene_dienste_backend/RaumbezogeneDiensteBackendApplication.java
@@ -10,6 +10,7 @@ public class RaumbezogeneDiensteBackendApplication {
 
     public static void main(String[] args) {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("my580dongpasswort!"));
 
         SpringApplication.run(RaumbezogeneDiensteBackendApplication.class, args);
     }
