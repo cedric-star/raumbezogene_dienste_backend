46b763c0c4aeba0d4ccea73b2c535e9f018e4aad
cedric-star <cdrc.wnsch@gmail.com>
2026-08-21 21:26:04 +0200

    vue

--- src/main/java/app/raumbezogene_dienste_backend/locations/gis/GisController.java
+++ src/main/java/app/raumbezogene_dienste_backend/locations/gis/GisController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -41,7 +38,7 @@ public class GisController {
         return ResponseEntity.ok(metadata);
     }
 
-    @GetMapping()
+    @PostMapping()
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public ResponseEntity<String> execGisFunction(@RequestBody String body) {
         return gisRepo.execGis(body);
